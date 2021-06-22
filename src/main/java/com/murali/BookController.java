49c32adfbc949b9c91ff6d1db6606f88b3d54732
mkodavati <murali_kodavati@intuit.com>
2021-06-22 21:39:32 +0530

    intial  commit

---
+++ src/main/java/com/murali/BookController.java
@@ -0,0 +1,30 @@
+package com.murali;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+
+    private final List<Book> bookList = new ArrayList<>();
+
+    @PostMapping
+    public ResponseEntity<Book> saveBook(@RequestBody Book book) {
+        bookList.add(book);
+        return new ResponseEntity<>(book, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> getBooks() {
+        return new ResponseEntity<>(bookList, HttpStatus.OK);
+    }
+}
