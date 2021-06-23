6835e0ff0bec67958e4c862b214032e47e5f71fa
mkodavati <murali_kodavati@intuit.com>
2021-06-23 14:11:03 +0530

    get book API Changes

--- src/main/java/com/murali/BookController.java
+++ src/main/java/com/murali/BookController.java
@@ -3,6 +3,7 @@ package com.murali;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,5 +27,11 @@ public class BookController {
     @GetMapping
     public ResponseEntity<List<Book>> getBooks() {
         return new ResponseEntity<>(bookList, HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Book> getBook(@PathVariable("id") Integer id) {
+        Book book = bookList.stream().filter(b -> b.getId() == id).findAny().orElseThrow(() -> new RuntimeException("book not found"));
+        return new ResponseEntity<>(book, HttpStatus.OK);
     }
 }
